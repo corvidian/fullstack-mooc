f71ee63afbd84c3aab3acf4e4143aee00cb465ae
Topias Heinonen <topias.heinonen@solinor.com>
2018-04-30 20:33:30 +0300

    Tehtävät 2.4 ja 2.5 tehty

--- t1/src/index.js
+++ t1/src/index.js
@@ -1,57 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Kurssi from './components/Kurssi'
 
 const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-      }
-    ]
-  }
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Kurssi kurssi={kurssi} />
+      {kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)}
     </div>
   )
 }
-
-const Kurssi = ({ kurssi }) => (
-  <div>
-    <Otsikko kurssi={kurssi.nimi} />
-    <Sisalto osat={kurssi.osat} />
-    <Yhteensa osat={kurssi.osat} />
-  </div>
-)
-
-const Otsikko = (props) => (
-  <h1>{props.kurssi}</h1>
-)
-
-const Sisalto = (props) => (
-  <div>
-    {props.osat.map((o) => <Osa key={o.nimi} osa={o} />)}
-  </div>
-)
-
-const Osa = ({osa}) => (
-  <p>{osa.nimi} {osa.tehtavia}</p>
-)
-
-const Yhteensa = (props) => (
-  <p>yhteensä {props.osat.map(o => o.tehtavia).reduce((a, b) => a + b)} tehtävää</p>
-)
 
 ReactDOM.render(
   <App />,
