dc27ae71442a4c5dbd991b30ebb34caf2023a7e8
Topias Heinonen <topias.heinonen@solinor.com>
2018-04-30 19:31:28 +0300

    Tehtävät 2.1 & 2.2 2.3 tehty

--- t1/src/index.js
+++ t1/src/index.js
@@ -22,12 +22,18 @@ const App = () => {
 
   return (
     <div>
-      <Otsikko kurssi={kurssi.nimi} />
-      <Sisalto osat={kurssi.osat} />
-      <Yhteensa osat={kurssi.osat} />
+      <Kurssi kurssi={kurssi} />
     </div>
   )
 }
+
+const Kurssi = ({ kurssi }) => (
+  <div>
+    <Otsikko kurssi={kurssi.nimi} />
+    <Sisalto osat={kurssi.osat} />
+    <Yhteensa osat={kurssi.osat} />
+  </div>
+)
 
 const Otsikko = (props) => (
   <h1>{props.kurssi}</h1>
@@ -35,12 +41,16 @@ const Otsikko = (props) => (
 
 const Sisalto = (props) => (
   <div>
-    {props.osat.map((o) => <p>{o.nimi} {o.tehtavia}</p>)}
+    {props.osat.map((o) => <Osa key={o.nimi} osa={o} />)}
   </div>
 )
 
+const Osa = ({osa}) => (
+  <p>{osa.nimi} {osa.tehtavia}</p>
+)
+
 const Yhteensa = (props) => (
-  <p>yhteensä {props.osat.map(o => o.tehtavia).reduce((a, b) => a + b, 0)} tehtävää</p>
+  <p>yhteensä {props.osat.map(o => o.tehtavia).reduce((a, b) => a + b)} tehtävää</p>
 )
 
 ReactDOM.render(
