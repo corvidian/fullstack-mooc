8f8cff0a0ab4058c5b24acc6d726325165d8eb6b
Topias Heinonen <topias.heinonen@gofore.com>
2022-04-07 19:52:13 +0300

    Tehtävä 1.1 Näin tämä oli kait ajateltu?

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -10,9 +10,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
@@ -21,9 +26,17 @@ const App = () => {
 const Header = (props) => <h1>{props.course}</h1>;
 
 const Content = (props) => (
-  <p>
-    {props.part} {props.exercises}
-  </p>
+  <>
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+  </>
 );
 
 const Total = (props) => <p>Number of exercises {props.total}</p>;
