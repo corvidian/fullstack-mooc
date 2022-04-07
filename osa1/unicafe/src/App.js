b2eaad5fba2ef2511623507474c7c6810c4bdcab
Topias Heinonen <topias.heinonen@gofore.com>
2022-04-07 22:16:02 +0300

    Tehtävä 1.6

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const App = () => {
+  // tallenna napit omiin tiloihinsa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={increaseGood} />
+      <Button text="neutral" handleClick={increaseNeutral} />
+      <Button text="bad" handleClick={increaseBad} />
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default App;
