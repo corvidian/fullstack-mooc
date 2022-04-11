db14e231e18bcd2c7ddbe7b1d299c77b507b842e
Topias Heinonen <topias.heinonen@gofore.com>
2022-04-11 20:55:49 +0300

    Tehtävä 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,9 +3,13 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterText, setFilterText] = useState("");
 
   const addEntry = (event) => {
     event.preventDefault();
@@ -28,10 +32,25 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (event) => {
+    setFilterText(event.target.value);
+  };
+
+  const shownEntries =
+    !filterText || filterText.length === 0
+      ? persons
+      : persons.filter((person) =>
+          new RegExp(filterText, "i").test(person.name)
+        );
 
   return (
     <>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with{" "}
+        <input value={filterText} onChange={handleFilterChange} />
+      </p>
+      <h3>Add a new entry</h3>
       <form onSubmit={addEntry}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -44,10 +63,12 @@ const App = () => {
         </div>
       </form>
 
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
 
-      {persons.map((person) => (
-        <p key={person.name}>{person.name} {person.number}</p>
+      {shownEntries.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </>
   );
