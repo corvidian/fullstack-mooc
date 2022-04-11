39482eddcae3e4e4d26b820460c96c61897218fd
Topias Heinonen <topias.heinonen@gofore.com>
2022-04-11 20:02:09 +0300

    Tehtävä 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addEntry = (event) => {
+    event.preventDefault();
+    const entry = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(entry));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addEntry}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+     
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+    </div>
+  );
+};
+
+export default App;
